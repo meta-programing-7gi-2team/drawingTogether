9a5e77068f28aa349464c168851d86066ac5c173
이민홍 <rntmf0220@gmail.com>
2024-08-13 10:11:41 +0900

    [Update] 업데이트 분리

--- Assets/3.Script/NetworkManager.cs
+++ Assets/3.Script/NetworkManager.cs
@@ -7,21 +7,28 @@ using Photon.Realtime;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private string testname;
-    [SerializeField] private InputField Roominput;
-    [SerializeField] private Button[] Room_Btu;
-    private RoomOptions RoomSetting = new RoomOptions { MaxPlayers = 8, IsVisible = true, IsOpen = true, EmptyRoomTtl = 0 };
+    public static NetworkManager instance = null;
 
     #region 서버 접속을 위한용도
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
         Connect();
     }
 
-    private void Update()
-    {
-        //ServerText.text = (PhotonNetwork.CountOfPlayers) + "명 접속중";
-    }
+    //private void Update()
+    //{
+    //    ServerText.text = (PhotonNetwork.CountOfPlayers) + "명 접속중";
+    //}
 
     public void Connect()
     {
@@ -36,34 +43,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby() // 로비 접속 완료되면 반환되는 메소드
     {
         Debug.Log("로비 연결");
-    }
-    #endregion
-
-    #region 방생성 관련
-    public void CreateRoom()
-    {
-        PhotonNetwork.CreateRoom(Roominput.text == "" ? "Room" + Random.Range(0, 100) : Roominput.text, RoomSetting);
-    }
-
-    public override void OnCreatedRoom() // 방생성에 성공하면 나오는 메소드
-    {
-        Debug.Log("방생성 완료");
-    }
-
-    public override void OnRoomListUpdate(List<RoomInfo> roomList) // 방이 새로 생성됬다면 호출되는 메소드
-    {
-        Debug.Log("Test");
-
-        for (int i = 0; i < roomList.Count && i < Room_Btu.Length; i++)
-        {
-            RoomInfo room = roomList[i];
-            Button roomButton = Room_Btu[i];
-
-
-            // 버튼의 텍스트 설정 (Button 아래에 있는 Text 컴포넌트를 찾아서 설정)
-            Text buttonText = roomButton.GetComponentInChildren<Text>();
-            buttonText.text = $"{room.Name} ({room.PlayerCount}/{room.MaxPlayers})";
-        }
     }
     #endregion
 
---
+++ Assets/3.Script/RoomManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private InputField Roominput;
+    [SerializeField] private Button[] Room_Btu;
+    private RoomOptions RoomSetting = new RoomOptions { MaxPlayers = 8, IsVisible = true, IsOpen = true, EmptyRoomTtl = 0 };
+
+
+    #region 방생성 관련
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(Roominput.text == "" ? "Room" + Random.Range(0, 100) : Roominput.text, RoomSetting);
+    }
+
+    public override void OnCreatedRoom() // 방생성에 성공하면 나오는 메소드
+    {
+        Debug.Log("방생성 완료");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList) // 방이 새로 생성됬다면 호출되는 메소드
+    {
+        Debug.Log("Test");
+
+        for (int i = 0; i < Room_Btu.Length; i++)
+        {
+            Button roomButton = Room_Btu[i];
+
+            Text RoomText = roomButton.transform.GetChild(0).GetComponent<Text>();
+            Text PlayerText = roomButton.transform.GetChild(1).GetComponent<Text>();
+
+            RoomText.text = string.Empty;
+            PlayerText.text = string.Empty;
+        }
+
+        for (int i = 0; i < roomList.Count && i < Room_Btu.Length; i++)
+        {
+            RoomInfo room = roomList[i];
+            Button roomButton = Room_Btu[i];
+
+
+            // 버튼의 텍스트 설정 (Button 아래에 있는 Text 컴포넌트를 찾아서 설정)
+            Text RoomText = roomButton.transform.GetChild(0).GetComponent<Text>();
+            Text PlayerText = roomButton.transform.GetChild(1).GetComponent<Text>();
+            RoomText.text = room.Name;
+            PlayerText.text = $"({room.PlayerCount}/{room.MaxPlayers})";
+        }
+    }
+    #endregion
+}
