774c166110fb100035313433012bec5be1aa1106
강유현 <soyuh1113@gmail.com>
2024-08-13 17:19:05 +0900

    [Update] 오디오 매니저 기본 틀 제작

--- Assets/3.Script/AudioManager.cs
+++ Assets/3.Script/AudioManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance = null;
+
+    [SerializeField] private AudioMixer audiomixer;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
 
     private void Awake()
     {
@@ -18,5 +24,43 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+    }
+
+    private void Start()
+    {
+        SetSfxVolume();
+
+        if (PlayerPrefs.HasKey("bgmVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetBgmVolume();
+            SetSfxVolume();
+        }
+    }
+
+    private void SetBgmVolume()
+    {
+        float volume = bgmSlider.value;
+        audiomixer.SetFloat("bgm", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("bgmVolume", volume);
+    }
+
+    private void SetSfxVolume()
+    {
+        float volume = sfxSlider.value;
+        audiomixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        bgmSlider.value = PlayerPrefs.GetFloat("bgmVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+
+        SetBgmVolume();
+        SetSfxVolume();
     }
 }
