2c00599b20e732c9de00ce97e2b869bd112174e2
강유현 <soyuh1113@gmail.com>
2024-08-13 13:21:21 +0900

    [Update] 로그인, 회원가입 기능 추가

---
+++ Assets/3.Script/User_Info/Setup_Login_Controller.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Setup_Login_Controller : MonoBehaviour
+{
+    [Header("Login")]
+    public TMP_InputField ID_input;
+    public TMP_InputField Password_input;
+    [SerializeField] private TextMeshProUGUI Log;
+    [SerializeField] private GameObject Login_UI;
+
+    [Header("SetUp")]
+    public TMP_InputField NickName_input;
+    public TMP_InputField id_input;
+    public TMP_InputField password_input;
+    [SerializeField] private TextMeshProUGUI S_Log;
+    [SerializeField] private GameObject SetUp_UI;
+
+    public void Login_Btn()
+    {
+        if (ID_input.text.Equals(string.Empty) || Password_input.text.Equals(string.Empty))
+        {
+            Log.text = "아이디와 비밀번호를 입력하세요.";
+            return;
+        }
+
+        if (UserInfo_Manager.instance.Login(ID_input.text, Password_input.text))
+        {
+            User_info info = UserInfo_Manager.instance.info;
+            Login_UI.SetActive(false);
+            SceneManager.LoadScene("Network");
+            Debug.Log(info.User_ID + " | " + info.User_Password + "로그인 성공!");
+        }
+        else
+        {
+            Log.text = "아이디와 비밀번호를 확인해주세요.";
+        }
+    }
+
+    public void SetActive_SetUpUI()
+    {
+        Login_UI.SetActive(false);
+        SetUp_UI.SetActive(true);
+    }
+
+    public void SetUp_Btn()
+    {
+        if (NickName_input.text.Equals(string.Empty) || id_input.text.Equals(string.Empty) || password_input.text.Equals(string.Empty))
+        {
+            S_Log.text = "닉네임과 아이디, 비밀번호를 입력하세요.";
+            return;
+        }
+
+        if (UserInfo_Manager.instance.SetUp(id_input.text, password_input.text, NickName_input.text))
+        {
+            User_info info = UserInfo_Manager.instance.info;
+            SetUp_UI.SetActive(false);
+            Login_UI.SetActive(true);
+        }
+        else
+        {
+            S_Log.text = "회원가입에 실패하셨습니다.";
+        }
+    }
+}
