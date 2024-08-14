6d14899d6d6c527fc4e1ef095ca3d7165bef5baa
이민홍 <rntmf0220@gmail.com>
2024-08-14 11:13:38 +0900

    [Update] 포톤 테스트 1차

--- Assets/3.Script/RoomManager.cs
+++ Assets/3.Script/RoomManager.cs
@@ -14,34 +14,30 @@ public class RoomManager : MonoBehaviourPunCallbacks
     [SerializeField] private Button[] Room_Btu;
     [SerializeField] private Text Nickname;
     [SerializeField] private Image UserImage;
-    [SerializeField] private int Player_Count = 8;
+    [SerializeField] private int Player_Count = 0;
 
     public override void OnJoinedLobby() // 로비 접속 완료되면 반환되는 메소드
     {
         Sprite playerSprite = Resources.Load<Sprite>($"Player_Image/{UserInfo_Manager.instance.info.User_Image}");
         UserImage.sprite = playerSprite;
-        Nickname.text = ($"{PhotonNetwork.NickName} 님");
+        Nickname.text = ($"{PhotonNetwork.NickName}");
     }
 
     #region 방관련
 
     public void Player_Count_T(int Player)
     {
-        Player_Count = Player;
+        PlayerPrefs.SetInt("Player_Count", Player);
     }
 
     public void CreateRoom()
     {
+        Player_Count = PlayerPrefs.GetInt("Player_Count");
+
         RoomOptions RoomSetting = new RoomOptions { MaxPlayers = Player_Count, IsVisible = true, IsOpen = true, EmptyRoomTtl = 0 };
 
         PhotonNetwork.CreateRoom(Roominput.text == "" ? "Room" + Random.Range(0, 100) : Roominput.text, RoomSetting);
 
-    }
-
-    public override void OnCreatedRoom() // 방생성에 성공하면 나오는 메소드
-    {
-        Debug.Log("방생성 완료");
-        SceneManager.LoadScene("IngameUI");
     }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList) // 방이 새로 생성되었다면 호출되는 메소드
@@ -83,13 +79,13 @@ public class RoomManager : MonoBehaviourPunCallbacks
         GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
         string roomName = clickedButton.transform.GetChild(0).GetComponent<Text>().text;
         PhotonNetwork.JoinRoom(roomName);
+        SceneManager.LoadScene("IngameUI");
     }
-    #endregion
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Successfully joined room: " + PhotonNetwork.CurrentRoom.Name);
-        PhotonNetwork.Instantiate("PhotonN", Vector3.zero, Quaternion.identity);
+        GameObject test = PhotonNetwork.Instantiate("PhotonN", Vector3.zero, Quaternion.identity);
     }
-
+    #endregion
 }
