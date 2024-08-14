669f1e03b0b015e7b011700cfcd92d92662eafb2
강유현 <soyuh1113@gmail.com>
2024-08-14 10:16:47 +0900

    [Update] 오디오 매니저 수정

--- Assets/3.Script/AudioManager.cs
+++ Assets/3.Script/AudioManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    #region 
+    private static AudioManager instance = null;
 
-    public static AudioManager instance = null;
+    [Header("Audio Scurce")]
+    [SerializeField] private AudioSource BGMSource;
+    [SerializeField] private AudioSource SFXSource;
 
-    [SerializeField] private AudioMixer audiomixer;
-    [SerializeField] private Slider bgmSlider;
-    [SerializeField] private Slider sfxSlider;
+    [Header("Audio Clip")]
+    public AudioClip Lobby_Background;
+    public AudioClip InGame_Background;
+    public AudioClip Timer_SFX;
+    public AudioClip BtnClick_SFX;
+    //public AudioClip Background;
 
     private void Awake()
     {
@@ -30,52 +33,12 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        SetSfxVolume();
-
-        if (PlayerPrefs.HasKey("bgmVolume"))
-        {
-            LoadVolume();
-        }
-        else
-        {
-            SetBgmVolume();
-            SetSfxVolume();
-        }
+        BGMSource.clip = Lobby_Background;
+        BGMSource.Play();
     }
 
-    private void SetBgmVolume()
+    public void PlaySFX(AudioClip clip)
     {
-        float volume = bgmSlider.value;
-        audiomixer.SetFloat("bgm", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("bgmVolume", volume);
+        SFXSource.PlayOneShot(clip);
     }
-
-    private void SetSfxVolume()
-    {
-        float volume = sfxSlider.value;
-        audiomixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("sfxVolume", volume);
-    }
-
-    private void LoadVolume()
-    {
-        bgmSlider.value = PlayerPrefs.GetFloat("bgmVolume");
-        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
-
-        SetBgmVolume();
-        SetSfxVolume();
-    }
-
-    #endregion
-
-    [Header("Audio Scurce")]
-    [SerializeField] private AudioSource BGMSource;
-    [SerializeField] private AudioSource SFXSource;
-
-    [Header("Audio Clip")]
-    public AudioClip Lobby_Background1;
-    public AudioClip InGame_Background;
-    public AudioClip Timer_SFX;
-    //public AudioClip Background;
-    //public AudioClip Background;
 }
