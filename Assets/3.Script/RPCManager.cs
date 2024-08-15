479090f82e6df598985dea4ecf59a522986f2f05
이민홍 <rntmf0220@gmail.com>
2024-08-15 15:55:39 +0900

    [Update] RPC 10차 테스트

--- Assets/3.Script/RPCManager.cs
+++ Assets/3.Script/RPCManager.cs
@@ -33,26 +33,28 @@ public class RPCManager : MonoBehaviourPunCallbacks
         userImage = NetworkManager.instance.GetPlayerImage(player);
 
         photonView.RPC("Room", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, PhotonNetwork.LocalPlayer.NickName, userImage);
+
+        photonView.RPC("Player_C", RpcTarget.All);
     }
 
-    //public override void OnPlayerEnteredRoom(Player newPlayer)
-    //{
-    //    string Player_I = NetworkManager.instance.GetPlayerImage(newPlayer);
-    //
-    //    // 새로 들어온 플레이어에게 기존 플레이어들의 정보를 전송
-    //    foreach (Player player in PhotonNetwork.PlayerList)
-    //    {
-    //        if (player == newPlayer)
-    //            continue; // 새로 들어온 플레이어는 건너뜀
-    //
-    //        string existingPlayerImage = NetworkManager.instance.GetPlayerImage(player);
-    //
-    //        photonView.RPC("Room", newPlayer, player.ActorNumber, player.NickName, existingPlayerImage);
-    //    }
-    //
-    //    // 모든 클라이언트에게 새 플레이어 정보를 전송
-    //    photonView.RPC("Room", RpcTarget.All, newPlayer.ActorNumber, newPlayer.NickName, Player_I);
-    //}
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        string Player_I = NetworkManager.instance.GetPlayerImage(newPlayer);
+    
+        // 새로 들어온 플레이어에게 기존 플레이어들의 정보를 전송
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if (player == newPlayer)
+                continue; // 새로 들어온 플레이어는 건너뜀
+    
+            string existingPlayerImage = NetworkManager.instance.GetPlayerImage(player);
+    
+            photonView.RPC("Room", newPlayer, player.ActorNumber, player.NickName, existingPlayerImage);
+        }
+    
+        // 모든 클라이언트에게 새 플레이어 정보를 전송
+        photonView.RPC("Room", RpcTarget.All, newPlayer.ActorNumber, newPlayer.NickName, Player_I);
+    }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
@@ -77,39 +79,40 @@ public class RPCManager : MonoBehaviourPunCallbacks
     }
 
     [PunRPC]
-    public void Room(int ActorNumber, string NickName, string image)
+    public void Player_C()
     {
-        //int playerIndex = ActorNumber - 1;
-        Debug.Log("Assigning player: " + NickName + " with ActorNumber: " + ActorNumber);
-
         for (int i = 0; i < seatObjects.Length; i++)
         {
-            Debug.Log("Checking seat " + i + " with childCount: " + seatObjects[i].transform.childCount);
             if (seatObjects[i].transform.childCount == 4)
             {
-                Debug.Log("Assigning " + NickName + " to seat " + i);
                 transform.SetParent(seatObjects[i].transform);
                 break;
             }
         }
+    }
 
-        //if (playerIndex < seatObjects.Length)
-        //{
-        //    GameObject seatObject = seatObjects[playerIndex];
-        //
-        //    Text playerNameText = seatObject.transform.GetChild(0).GetComponent<Text>();
-        //    Image playerImage = seatObject.transform.GetChild(1).GetComponent<Image>();
-        //    Sprite playerSprite = Resources.Load<Sprite>($"Player_Image/{image}");
-        //    Color color = playerImage.color;
-        //    color.a = 1;
-        //    playerImage.color = color;
-        //
-        //    playerNameText.text = NickName;
-        //    playerImage.sprite = playerSprite;
-        //}
-        //else
-        //{
-        //    Debug.Log("No available seat for player " + NickName);
-        //}
+    [PunRPC]
+    public void Room(int ActorNumber, string NickName, string image)
+    {
+        int playerIndex = ActorNumber - 1;
+
+        if (playerIndex < seatObjects.Length)
+        {
+            GameObject seatObject = seatObjects[playerIndex];
+        
+            Text playerNameText = seatObject.transform.GetChild(0).GetComponent<Text>();
+            Image playerImage = seatObject.transform.GetChild(1).GetComponent<Image>();
+            Sprite playerSprite = Resources.Load<Sprite>($"Player_Image/{image}");
+            Color color = playerImage.color;
+            color.a = 1;
+            playerImage.color = color;
+        
+            playerNameText.text = NickName;
+            playerImage.sprite = playerSprite;
+        }
+        else
+        {
+            Debug.Log("No available seat for player " + NickName);
+        }
     }
 }
