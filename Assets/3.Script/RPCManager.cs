9a0d10fbff0536c82ce418f7814ddb44124ddfc6
이민홍 <rntmf0220@gmail.com>
2024-08-15 15:47:02 +0900

    [Update] RPC 9차 테스트

--- Assets/3.Script/RPCManager.cs
+++ Assets/3.Script/RPCManager.cs
@@ -16,12 +16,6 @@ public class RPCManager : MonoBehaviourPunCallbacks
     {
         seatObjects = GameObject.FindGameObjectsWithTag("Player_Room");
 
-        Player player = PhotonNetwork.LocalPlayer;
-
-        userImage = NetworkManager.instance.GetPlayerImage(player);
-
-        photonView.RPC("Room", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, PhotonNetwork.LocalPlayer.NickName, userImage);
-
         int RoomMax = PhotonNetwork.CurrentRoom.MaxPlayers;
 
         for (int i = 0; i < RoomMax; i++)
@@ -33,38 +27,37 @@ public class RPCManager : MonoBehaviourPunCallbacks
         {
             seatObjects[i].SetActive(false);
         }
+
+        Player player = PhotonNetwork.LocalPlayer;
+
+        userImage = NetworkManager.instance.GetPlayerImage(player);
+
+        photonView.RPC("Room", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, PhotonNetwork.LocalPlayer.NickName, userImage);
     }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        Debug.Log("New player joined. ActorNumber: " + newPlayer.ActorNumber + ", NickName: " + newPlayer.NickName);
-
-        string Player_I = NetworkManager.instance.GetPlayerImage(newPlayer);
-
-        // 새로 들어온 플레이어에게 기존 플레이어들의 정보를 전송
-        foreach (Player player in PhotonNetwork.PlayerList)
-        {
-            if (player == newPlayer)
-                continue; // 새로 들어온 플레이어는 건너뜀
-
-            string existingPlayerImage = NetworkManager.instance.GetPlayerImage(player);
-            photonView.RPC("Room", newPlayer, player.ActorNumber, player.NickName, existingPlayerImage);
-        }
-
-        // 모든 클라이언트에게 새 플레이어 정보를 전송
-        photonView.RPC("Room", RpcTarget.All, newPlayer.ActorNumber, newPlayer.NickName, Player_I);
-    }
+    //public override void OnPlayerEnteredRoom(Player newPlayer)
+    //{
+    //    string Player_I = NetworkManager.instance.GetPlayerImage(newPlayer);
+    //
+    //    // 새로 들어온 플레이어에게 기존 플레이어들의 정보를 전송
+    //    foreach (Player player in PhotonNetwork.PlayerList)
+    //    {
+    //        if (player == newPlayer)
+    //            continue; // 새로 들어온 플레이어는 건너뜀
+    //
+    //        string existingPlayerImage = NetworkManager.instance.GetPlayerImage(player);
+    //
+    //        photonView.RPC("Room", newPlayer, player.ActorNumber, player.NickName, existingPlayerImage);
+    //    }
+    //
+    //    // 모든 클라이언트에게 새 플레이어 정보를 전송
+    //    photonView.RPC("Room", RpcTarget.All, newPlayer.ActorNumber, newPlayer.NickName, Player_I);
+    //}
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         Debug.Log(otherPlayer);
         photonView.RPC("LeftRoom", RpcTarget.All, otherPlayer.ActorNumber);
-    }
-
-    public override void OnLeftRoom()
-    {
-        Debug.Log("ddd");
-        //photonView.RPC("LeftRoom", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber);
     }
 
     [PunRPC]
@@ -86,36 +79,37 @@ public class RPCManager : MonoBehaviourPunCallbacks
     [PunRPC]
     public void Room(int ActorNumber, string NickName, string image)
     {
-        int playerIndex = ActorNumber - 1;
-
-        Debug.Log(playerIndex);
+        //int playerIndex = ActorNumber - 1;
+        Debug.Log("Assigning player: " + NickName + " with ActorNumber: " + ActorNumber);
 
         for (int i = 0; i < seatObjects.Length; i++)
         {
-            if (seatObjects[i].transform.childCount.Equals(4))
+            Debug.Log("Checking seat " + i + " with childCount: " + seatObjects[i].transform.childCount);
+            if (seatObjects[i].transform.childCount == 4)
             {
+                Debug.Log("Assigning " + NickName + " to seat " + i);
                 transform.SetParent(seatObjects[i].transform);
                 break;
             }
         }
 
-        if (playerIndex < seatObjects.Length)
-        {
-            GameObject seatObject = seatObjects[playerIndex];
-
-            Text playerNameText = seatObject.transform.GetChild(0).GetComponent<Text>();
-            Image playerImage = seatObject.transform.GetChild(1).GetComponent<Image>();
-            Sprite playerSprite = Resources.Load<Sprite>($"Player_Image/{image}");
-            Color color = playerImage.color;
-            color.a = 1;
-            playerImage.color = color;
-
-            playerNameText.text = NickName;
-            playerImage.sprite = playerSprite;
-        }
-        else
-        {
-            Debug.Log("No available seat for player " + NickName);
-        }
+        //if (playerIndex < seatObjects.Length)
+        //{
+        //    GameObject seatObject = seatObjects[playerIndex];
+        //
+        //    Text playerNameText = seatObject.transform.GetChild(0).GetComponent<Text>();
+        //    Image playerImage = seatObject.transform.GetChild(1).GetComponent<Image>();
+        //    Sprite playerSprite = Resources.Load<Sprite>($"Player_Image/{image}");
+        //    Color color = playerImage.color;
+        //    color.a = 1;
+        //    playerImage.color = color;
+        //
+        //    playerNameText.text = NickName;
+        //    playerImage.sprite = playerSprite;
+        //}
+        //else
+        //{
+        //    Debug.Log("No available seat for player " + NickName);
+        //}
     }
 }
